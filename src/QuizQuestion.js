8fbd844e7fb26d8b5f283ab0d6a5e862eb55f279
Niraj <niraj.kumarz@aol.com>
2018-12-29 20:13:48 -0600

    Update QuizQuestion.js

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,14 +3,14 @@ import QuizQuestionButton from "./QuizQuestionButton";
 
 class QuizQuestion extends Component {
   
-  handleClick(buttonText) {
-    if (buttonText === this.props.quiz_question.answer) {
-        this.setState({ incorrectAnswer: false })
-        this.props.showNextQuestionHandler()
-      } else {
-        this.setState({ incorrectAnswer: true })
+    handleClick(button_text) {
+        if (button_text === this.props.quiz_question.answer) {
+          this.setState({ incorrectAnswer: false })
+          this.props.showNextQuestionHandler()
+        } else {
+          this.setState({ incorrectAnswer: true })
+        }
       }
-  }
 
   render() {
     return (
